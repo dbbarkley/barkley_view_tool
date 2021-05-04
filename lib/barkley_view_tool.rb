13b34691b48a2b91f35a0b95efa058aa3f51f088
Daniel Barkley <dbbarkley93@gmail.com>
2021-05-03 23:20:33 -0500

    tool initial commit

---
+++ lib/barkley_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "barkley_view_tool/version"
+require_relative "barkley_view_tool/renderer"
+
+module BarkleyViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
