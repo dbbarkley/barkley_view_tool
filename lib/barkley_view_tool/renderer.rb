8937776b89f7a22f01ed1e93660b74a8463ff8b6
Daniel Barkley <dbbarkley93@gmail.com>
2021-05-03 23:27:53 -0500

    updated read me and gemspec

--- lib/barkley_view_tool/renderer.rb
+++ lib/barkley_view_tool/renderer.rb
@@ -1,7 +1,7 @@
 module BarkleyViewTool
-    class Renderer
-      def self.copyright name, msg
-        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-      end
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
     end
   end
+end
